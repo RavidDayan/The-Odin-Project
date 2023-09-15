0c0fbf2d95bf5cb2e71c6b9170b96f96f55cd719
Ravid Dayan <dayanravid@gmail.com>
2023-09-15 03:43:37 +0300

    finshed darken and rainbow buttons 1.each click disables the other

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,49 +1,74 @@
 
 //elements
-const slider=document.getElementById("slider");
-const container=document.getElementById("container");
+const slider = document.getElementById("slider");
+const container = document.getElementById("container");
+const clearButton = document.getElementById("reset");
+const darkenButton = document.getElementById("darken");
+const rainbowButton = document.getElementById("rainbow");
+let darkenBox = true;
+let randomlyColorBox = false;
 
-//initilazation
+//initilazation and listeners
 let numberOfSquares = getNumberOfSquares();
 container.appendChild(BoxContainer(numberOfSquares));
-
-slider.addEventListener("input",()=>{
+slider.addEventListener("input", () => {
     container.removeChild(container.firstChild);
-    console.log(slider);
     let numberOfSquares = getNumberOfSquares();
     container.appendChild(BoxContainer(numberOfSquares));
-
+})
+darkenButton.addEventListener("click",()=>{
+    darkenBox=true;
+    randomlyColorBox=false;
+})
+rainbowButton.addEventListener("click",()=>{
+    darkenBox=false;
+    randomlyColorBox=true;
+    console.log(randomlyColorBox)
 })
 //functions
-function getNumberOfSquares(){
+function getNumberOfSquares() {
     let num = slider.value;
     return num;
 }
-numberOfSquares=getNumberOfSquares();
-
-function BoxContainer(numberOfSquares){
-    let boxContainer=document.createElement("div");
+function BoxContainer(numberOfSquares) {
+    let boxContainer = document.createElement("div");
     boxContainer.className = "box-container";
-    for(let i=0;i<numberOfSquares*numberOfSquares;i++){
+    for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
         boxContainer.appendChild(Box(numberOfSquares));
     }
     return boxContainer;
 }
-function Box(numberOfSquares){
-    let box=document.createElement("div");
+function Box(numberOfSquares) {
+    let box = document.createElement("div");
     box.className = "box";
-    box.style.opacity=1;
-    box.style.width = (1/numberOfSquares)*100+"%";
-    box.style.height = (1/numberOfSquares)*100+"%";
-    box.addEventListener("mouseenter",()=>{
-        darken(box);
+    box.style.opacity = 1;
+    box.style.width = (1 / numberOfSquares) * 100 + "%";
+    box.style.height = (1 / numberOfSquares) * 100 + "%";
+    box.addEventListener("mouseenter", () => {
+        if(darkenBox){
+            darken(box);
+        }
+        if(randomlyColorBox){
+            colorBoxRandomly(box);
+        }
+        
     })
     return box;
 }
-function darken(box){
-    let opacity=box.style.opacity;
-    opacity=opacity-0.1
+function darken(box) {
+    let opacity = box.style.opacity;
+    opacity = opacity - 0.1
     console.log(opacity);
-    box.style.opacity=opacity;
+    box.style.opacity = opacity;
+}
+function GenerateRandomColor() {
+    return Math.floor(Math.random() * 255);
+}
+function colorBoxRandomly(box) {
+    let red = GenerateRandomColor();
+    let green = GenerateRandomColor();
+    let blue = GenerateRandomColor();
+    const rgbColor = "rgb("+red+","+green+","+blue+")";
+    box.style.backgroundColor=rgbColor;
 }
 

179e6ca4dcc79c40a685ffaa27384f4862c30064
Ravid Dayan <dayanravid@gmail.com>
2023-09-16 00:44:13 +0300

    started the script 1.constants of index elements 2.constructor for book+eventlistner 3.clear the shelf gfrom books 4.add all books in array to shelf

---
+++ library/script.js
@@ -0,0 +1,31 @@
+const myLibrary=[];
+const shelf=document.getElementById("library");
+const addButton=document.getElementById("addButton")
+const removeButton=document.getElementById("removeButton")
+const bookName=document.getElementById("name");
+const bookAuthor=document.getElementById("author");
+const bookRead=document.getElementById("read");
+function Book(name,author,read){
+    this.name=name;
+    this.author=author;
+    this.read=read;
+    this.book = document.createElement("div");
+    this.book.classList.add("book");
+    this.book.addEventListener("click",()=>{
+
+        bookName.textContent= name;
+        bookAuthor.textContent= author;
+        bookRead.textContent= read;
+    })
+}
+function clearShelf(){
+    while (shelf.firstChild) {
+        shelf.removeChild(shelf.firstChild);
+      }
+}
+function displayBooks(){
+    clearShelf();
+    myLibrary.forEach(book => {
+        shelf.appendChild(book.book);
+    });
+}

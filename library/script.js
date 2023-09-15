06e1f45e1c3d0c104c506ed1a56d566e70efa13d
Ravid Dayan <dayanravid@gmail.com>
2023-09-16 01:24:29 +0300

    added procces form function added add new book to library

--- library/script.js
+++ library/script.js
@@ -5,6 +5,7 @@ const removeButton=document.getElementById("removeButton")
 const bookName=document.getElementById("name");
 const bookAuthor=document.getElementById("author");
 const bookRead=document.getElementById("read");
+
 function Book(name,author,read){
     this.name=name;
     this.author=author;
@@ -28,4 +29,15 @@ function displayBooks(){
     myLibrary.forEach(book => {
         shelf.appendChild(book.book);
     });
+}
+function addNewBook(book){
+    myLibrary.push(book);
+    displayBooks()
+}
+function processForm(){
+    const formName = document.getElementById(bookName);
+    const formAuthor = document.getElementById(bookAuthor);
+    const formRead = document.getElementById(bookRead);
+    const newBook=new Book(formName,formAuthor,formRead);
+
 }

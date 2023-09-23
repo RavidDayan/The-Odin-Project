6d2b9b0620d8907cc6e36e8df906d1c8d615266a
Ravid Dayan <dayanravid@gmail.com>
2023-09-23 15:33:42 +0300

    made the work class

--- cvGenerator/src/components/WorkClass.jsx
+++ cvGenerator/src/components/WorkClass.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-const workList=[];
+const workList = [];
 
-class work{
-    constructor(){
-this.key=uuidv4();
+class work {
+    constructor(name, position, summary, dareFrom, dateTo) {
+        this.key = uuidv4();
+        this.name = name;
+        this.position = position;
+        this.summary = summary;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
     }
 }

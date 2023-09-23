9deeda4ef8b1680f1450bedbc3ff0675b3adcfcc
Ravid Dayan <dayanravid@gmail.com>
2023-09-24 01:03:06 +0300

    cv space will control school and work list and have a delete function and add for each sectio

--- cvGenerator/src/components/CvSpace.jsx
+++ cvGenerator/src/components/CvSpace.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Work, { workList } from './WorkClass.jsx'
+import WorkItem from './WorkItem.jsx';
 
 function CvSpace() {
-    return (
-        <div className='cvSpace'>
+    const [workItems, updateWorkItems] = useState(workList);
 
+    function Delete(type, item) {
+        if (type == "work") {
+            updateWorkItems(workItems.filter(x => { x.key = item.key }));
+        }
+        if (type == "school") {
+            console.log("delete school");
+        }
+    }
+    function AddButtonFunc(){
+        
+    }
+
+
+    return (
+
+        <div className='cvSpace'>
+            {workItems.map(x => {
+                return <WorkItem workDetails={x} deleteFunc={Delete} />;
+            })}
         </div>
     );
-    
+
 }
 
 export default CvSpace;
